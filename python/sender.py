93591996a531849b29b629c5b30a49cdc92ad726
croossin <croossin94@gmail.com>
2016-03-03 11:11:44 -0600

    Update spammer - 10 messages then sleep for 2 seconds

--- python/sender.py
+++ python/sender.py
@@ -15,7 +15,8 @@ class Send(riffle.Domain):
     def onJoin(self):
     	
         while True:
-        	self.publish("temp", {'module': random.choice(modules), 'data': str(random.random()), 'sensor': random.choice(sensors),'id': str(random.randrange(0,144)), 'type': random.choice(types), 'location': random.choice(locations)})
+        	for x in range(10):
+	        	self.publish("temp", {'module': random.choice(modules), 'data': str(random.random()), 'sensor': random.choice(sensors),'id': str(random.randrange(0,144)), 'type': random.choice(types), 'location': random.choice(locations)})
         	time.sleep(2)
 
 if __name__ == '__main__':
